564a00221f1ed198efb5c86b051c12f256a7663b
rafaelccorrea <rafaelgustavocorrea@gmail.com>
2024-05-26 02:17:32 -0300

    add router callback

--- src/auth/guard/facebook.strategy.ts
+++ src/auth/guard/facebook.strategy.ts
@@ -10,7 +10,7 @@ export class FacebookStrategy extends PassportStrategy(Strategy, 'facebook') {
     super({
       clientID: process.env.FACEBOOK_APP_ID,
       clientSecret: process.env.FACEBOOK_APP_SECRET,
-      callbackURL: 'http://localhost:3000/auth/facebook/callback',
+      callbackURL: process.env.ROUTER_CALLBACK_URL,
       profileFields: ['id', 'emails', 'name'],
     });
   }
